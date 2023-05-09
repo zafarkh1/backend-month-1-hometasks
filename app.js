2716bd74fe8d8810d6895771972d47e24703956f
zafarkh <hatamovzafar7070@gmail.com>
2023-05-09 19:16:27 +0500

    Done!

--- app.js
+++ app.js
@@ -1,45 +1,29 @@
-//                        task-1
-// const word1 = "#123adfdgth6sdfs%$#(2345gdfngfFGJHGHG34dfsmklfm@#$%$#/]defg";
-// const sortedWord = word1
-//   .split("").sort((a, b) => {
-// 		if(isNaN(a) && isNaN(b)) {
-// 			console.log(a.localeCompare(b));
-// 		}
-// 	})
-
-// console.log(sortedWord); // Output: adfdgthdfsdfsgdfngffFGJHGHGdfsmklfm1234562345
-
-//                         task-2
-
-// let word = "kiritilgan matnni hamma sozlarini bosh harflarga aylantiring";
-
-// function capitalize(arg) {
-// 	let words = arg.split(" ")
-// 	for(let i = 0; i < words.length; i++) {
-// 		let word = words[i]
-// 		words[i] = word.charAt(0).toUpperCase() + word.slice(1);
-// 	}
-// 	return words.join(' ')
+//                       task-1
+// function largestEven(arg) {
+//   let max = arg[arg.length - 1];
+//   arg[0] = arg[0] < max && max % 2 == 0 ? max : arg[0];
+//   arg.pop();
+//   if (arg.length > 1) return largestEven(arg);
+//   return arg[0] % 2 == 0 ? arg[0] : -1;
 // }
 
-// console.log(capitalize(word));
+// console.log(largestEven([3, 7, 29, 1, 7, 9, 10, 13]));
 
-//                         task-3
-// let numbers = 3621;
-// let sum = 0
-// for (let i = 0; i < numbers.toString().length; i++) {
-// 	sum = sum + parseInt(numbers.toString()[i] ** [i+1]);
+//                        task-2
+// function collatz(arg, count = 0) {
+//   arg = arg % 2 ? arg * 3 + 1 : parseInt(arg / 2);
+//   count++;
+//   if (arg == 1) return count;
+//   return collatz(arg, count);
 // }
-// console.log(sum);
 
-//                    task-4
-// let text =
-//   "Lorem, ipsum dolor piss off amet consectetur adipisicing elit. Laudantium quo quas corrupti, magni quia hell, cumque adipisci id harum esse sheet iure provident sunt, debitis voluptates aliquam? Provident, unde qui.";
+// console.log(collatz(3));
 
-// let splitted = text.split(" ");
-// let swearWords = ["hell", "pissed", "sheet"];
-// for (let i = 0; i < swearWords.length; i++) {
-//   let pattern = new RegExp(swearWords[i], "gi");
-//   console.log(pattern);
-//   text = text.replace(pattern, "***");
+//                       task-3
+// function integerToString (num, base, res = '') {
+// 	res = num % base
+// 	num = parseInt(num % base)
+// 	if(num != 0) return integerToString (num, base, res)
+// 	return res
 // }
+// console.log(integerToString(10, 2))
